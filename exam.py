b2596bdf1cc01c692b9a480d45e54ae56ec239c6
Stock05 <qace756@gmail.com>
2024-03-13 11:43:45 +0900

    검색 기능 적용

--- exam.py
+++ exam.py
@@ -25,3 +25,53 @@ from langchain_core.output_parsers import StrOutputParser
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 chain.invoke({"input": "2024년 청년 지원 정책에 대해 알려줘"})
+
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://www.moel.go.kr/policy/policyinfo/support/list4.do")
+
+docs = loader.load()
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+# 바로 Docs 내용을 반영도 가능합니다.
+from langchain_core.documents import Document
+
+document_chain.invoke({
+    "input": "국민취업지원제도가 뭐야",
+    "context": [Document(page_content="""국민취업지원제도란?
+
+취업을 원하는 사람에게 취업지원서비스를 일괄적으로 제공하고 저소득 구직자에게는 최소한의 소득도 지원하는 한국형 실업부조입니다. 2024년부터 15~69세 저소득층, 청년 등 취업취약계층에게 맞춤형 취업지원서비스와 소득지원을 함께 제공합니다.
+[출처] 2024년 달라지는 청년 지원 정책을 확인하세요.|작성자 정부24""")]
+})
+
+from langchain.chains import create_retrieval_chain
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "국민취업지원제도가 뭐야"})
+print(response["answer"])
+
+# LangSmith offers several features that can help with testing:...
